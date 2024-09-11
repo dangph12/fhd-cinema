754d40bbab18b70f65ad083a74030e11619e90fa
lnvh <lenguyenviethung1509@gmail.com>
2024-09-11 20:44:37 +0700

    chore: add new name of member

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,32 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import NavBar from './components/NavBar';
-import SlideBar from './components/SlideBar';
-import SlideShow from './components/SlideShow'
-import Banner from './components/Banner';
-import Footer from './components/Footer';
-import BodyFirst from "./components/BodyFirst";
-import BodySecond from "./components/BodySecond";
-import StarMember from "./components/StarMember";
-import BannerSecond from "./components/BannerSecond";
+import MovieCard from "./components/Seats/MovieCard";
+import SeatSelection from "./components/Seats/SeatSelection";
+// import SlideBar from './components/SlideBar';
+// import SlideShow from './components/SlideShow'
+// import Banner from './components/Banner';
+// import Footer from './components/Footer';
+// import BodyFirst from "./components/BodyFirst";
+// import BodySecond from "./components/BodySecond";
+// import StarMember from "./components/StarMember";
+// import BannerSecond from "./components/BannerSecond";
 
 
 function App() {
   return (
     <div className="App">
         <NavBar/>
-        <SlideShow/>
+        <MovieCard/>
+        <SeatSelection/>
+        {/* <SlideShow/>
         <SlideBar/>
         <Banner/> 
         <BodyFirst/>
         <BannerSecond/>
         <BodySecond/>
         <StarMember/> 
-        <Footer/>
+        <Footer/> */}
     </div>
   );
 }
