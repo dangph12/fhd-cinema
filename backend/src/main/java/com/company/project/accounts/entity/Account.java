b487a96cdb6bd9b65aa2bc449a5986de657073d8
dangph12 <dangphhe181217@fpt.edu.vn>
2024-09-23 20:20:25 +0700

    fix: Use builder lombok (#24)

--- backend/src/main/java/com/company/project/accounts/entity/Account.java
+++ backend/src/main/java/com/company/project/accounts/entity/Account.java
@@ -1,10 +1,7 @@
 package com.company.project.accounts.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Entity
@@ -13,6 +10,7 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @Table(name = "accounts")
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
 public class Account {
 
     @Id
