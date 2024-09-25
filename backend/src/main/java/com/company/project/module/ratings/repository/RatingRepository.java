3ec9b6f39cfff6612a54c37081514959c692e62c
ducndahe181048 <144119087+ducndahe181048@users.noreply.github.com>
2024-09-25 17:33:00 +0700

    fix: add @Repository for rating

--- backend/src/main/java/com/company/project/module/ratings/repository/RatingRepository.java
+++ backend/src/main/java/com/company/project/module/ratings/repository/RatingRepository.java
@@ -2,7 +2,9 @@ package com.company.project.module.ratings.repository;
 
 import com.company.project.module.ratings.entity.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RatingRepository extends JpaRepository<Rating, String> {
     boolean existsByRatingId(String ratingId);
 }
