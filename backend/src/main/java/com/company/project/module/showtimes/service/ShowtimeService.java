c9c543c6684d3b110f60d5e3e4ef9ee8da172c30
ducndahe181048 <144119087+ducndahe181048@users.noreply.github.com>
2024-09-25 23:03:52 +0700

    fix: HTTP method of showtime

--- backend/src/main/java/com/company/project/module/showtimes/service/ShowtimeService.java
+++ backend/src/main/java/com/company/project/module/showtimes/service/ShowtimeService.java
@@ -1,6 +1,13 @@
 package com.company.project.module.showtimes.service;
 
 import com.company.project.common.Status;
+import com.company.project.module.movies.entity.Movie;
+import com.company.project.module.movies.exception.MovieException;
+import com.company.project.module.movies.repository.MovieRepository;
+import com.company.project.module.ratings.entity.Rating;
+import com.company.project.module.screens.entity.Screen;
+import com.company.project.module.screens.exception.ScreenException;
+import com.company.project.module.screens.repository.ScreenRepository;
 import com.company.project.module.showtimes.common.ShowtimeStatusMessage;
 import com.company.project.module.showtimes.dto.request.ShowtimeCreationRequest;
 import com.company.project.module.showtimes.dto.request.ShowtimeUpdateRequest;
@@ -15,10 +22,19 @@ import java.util.List;
 public class ShowtimeService {
 
     private final ShowtimeRepository showtimeRepository;
+    private final MovieRepository movieRepository;
+    private final ScreenRepository screenRepository;
 
-    public ShowtimeService(ShowtimeRepository showtimeRepository) {
+//    public ShowtimeService(ShowtimeRepository showtimeRepository) {
+//        this.showtimeRepository = showtimeRepository;
+//    }
+
+    public ShowtimeService(ShowtimeRepository showtimeRepository, MovieRepository movieRepository, ScreenRepository screenRepository) {
         this.showtimeRepository = showtimeRepository;
+        this.movieRepository = movieRepository;
+        this.screenRepository = screenRepository;
     }
+
 
     public List<Showtime> getAllShowtimes() {
         return showtimeRepository.findAll();
@@ -29,7 +45,16 @@ public class ShowtimeService {
     }
 
     public Showtime createShowtime(ShowtimeCreationRequest request) {
+
+        Movie movie = movieRepository.findById(request.getMovieId())
+                .orElseThrow(() -> new MovieException(Status.FAIL.getValue(), "Invalid Movie ID"));
+
+        Screen screen = screenRepository.findById(request.getScreenId())
+                .orElseThrow(() -> new ScreenException(Status.FAIL.getValue(), "Invalid Screen ID"));
+
         Showtime showtime = Showtime.builder()
+                .movie(movie)
+                .screen(screen)
                 .showtimePrice(request.getShowtimePrice())
                 .showtimeAt(request.getShowtimeAt())
                 .build();
