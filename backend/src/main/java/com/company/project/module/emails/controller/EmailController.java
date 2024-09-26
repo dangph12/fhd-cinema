d9ec23aecf207e57260e5f32afea5dde0d6bb20e
namdng09 <namduong090104@gmail.com>
2024-09-26 07:51:45 +0700

    Pulled changes from feature/receive-ticket-via-email without commits

--- backend/src/main/java/com/company/project/module/emails/controller/EmailController.java
+++ backend/src/main/java/com/company/project/module/emails/controller/EmailController.java
@@ -1,10 +1,19 @@
 package com.company.project.module.emails.controller;
 
 
-import com.company.project.module.emails.service.SendEmailService;
+import jakarta.validation.Valid;
+
+import com.company.project.common.ApiResponse;
+import com.company.project.common.Status;
+import com.company.project.email.common.EmailStatusMessage;
+import com.company.project.email.dto.request.EmailBillRequest;
+import com.company.project.email.dto.request.EmailCreationRequest;
+import com.company.project.email.service.EmailService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +22,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailController{
 
   @Autowired
-  private SendEmailService sendEmailService;
+  private EmailService emailService;
 
-  @GetMapping
-  public String sendEmail() {
-    sendEmailService.sendEmail("fhdcinemavietnam@gmail.com", "Test body", "Test subject");
-    return "Sent successfully";
+  @GetMapping("/confirmation")
+  ResponseEntity<ApiResponse<Void>> sendPurchaseConfirmationEmail(@Valid @RequestBody EmailCreationRequest request) {
+    emailService.sendEmailConfirmation(request);
+
+    return ResponseEntity.ok().body(ApiResponse.<Void>builder()
+      .status(Status.SUCCESS.getValue())
+      .message(EmailStatusMessage.EMAIL_SENT_SUCCESSFULLY.getMessage())
+      .build());
   }
+
+  @GetMapping("/bill")
+  ResponseEntity<ApiResponse<Void>> sendBillInformationEmail(@Valid @RequestBody EmailBillRequest request) {
+    emailService.sendEmailBill(request);
+
+    return ResponseEntity.ok().body(ApiResponse.<Void>builder()
+      .status(Status.SUCCESS.getValue())
+      .message(EmailStatusMessage.EMAIL_SENT_SUCCESSFULLY.getMessage())
+      .build());
+  }
+
 }
