725d2871a9dd940bd271e30708f8f1ccfc026956
dangph12 <dangphhe181217@fpt.edu.vn>
2024-09-27 18:29:47 +0700

    feat: Update create method

--- admin-frontend/src/app/(admin)/tables/accounts-tables/page.jsx
+++ admin-frontend/src/app/(admin)/tables/accounts-tables/page.jsx
@@ -1,56 +1,9 @@
 import PageMetaData from '@/components/PageMetaData'
-import CustomComponentContainerCard from '@/components/CustomComponentContainerCard'
 import ReactTable from '@/components/Table'
-import { Col, FormCheck, Row, Card, CardBody, CardHeader, CardTitle, Button } from 'react-bootstrap'
+import { Col, Row, Card, CardBody, CardHeader, CardTitle, Button, Form, Modal } from 'react-bootstrap'
 import { useEffect, useState } from 'react'
 
 const AccountsTables = () => {
-  const handleUpdate = (id) => {
-    // Implement the update logic here
-    console.log(`Update account with id: ${id}`)
-  }
-
-  const handleDelete = (id) => {
-    // Implement the update logic here
-    console.log(`Delete account with id: ${id}`)
-  }
-
-  const CustomerDetailTable = ({ accounts }) => {
-    const pageSizeList = [2, 5, 10, 20, 50]
-    return (
-      <Card>
-      <CardHeader>
-        <Row className="align-items-center">
-        <Col>
-          <CardTitle as="h4">Accounts Details</CardTitle>
-        </Col>
-        <Col className="text-end">
-          <Button
-          className="btn btn-primary"
-          onClick={() => {
-            // Implement the create logic here
-            console.log('Create new account')
-          }}
-          >
-          Create account
-          </Button>
-        </Col>
-        </Row>
-      </CardHeader>
-      <CardBody className="pt-0">
-        <ReactTable
-        columns={columns}
-        data={accounts}
-        rowsPerPageList={pageSizeList}
-        pageSize={10}
-        tableClass="datatable"
-        theadClass="table-light"
-        showPagination
-        />
-      </CardBody>
-      </Card>
-    )
-  }
   const [accounts, setAccounts] = useState()
 
   useEffect(() => {
@@ -66,6 +19,37 @@ const AccountsTables = () => {
   }
 
   const filteredAccounts = accounts?.filter((account) => account.accountName.toLowerCase().includes(searchTerm.toLowerCase()))
+
+  const CustomerDetailTable = ({ accounts }) => {
+    const pageSizeList = [2, 5, 10, 20, 50]
+    return (
+      <Card>
+        <CardHeader>
+          <Row className="align-items-center">
+            <Col>
+              <CardTitle as="h4">Accounts Details</CardTitle>
+            </Col>
+            <Col className="text-end">
+              <Button className="btn btn-primary" onClick={() => openCreateModal()}>
+                Create account
+              </Button>
+            </Col>
+          </Row>
+        </CardHeader>
+        <CardBody className="pt-0">
+          <ReactTable
+            columns={columns}
+            data={accounts}
+            rowsPerPageList={pageSizeList}
+            pageSize={10}
+            tableClass="datatable"
+            theadClass="table-light"
+            showPagination
+          />
+        </CardBody>
+      </Card>
+    )
+  }
 
   const columns = [
     {
@@ -84,18 +68,9 @@ const AccountsTables = () => {
           original: { accountId },
         },
       }) => (
-        <button
-          onClick={() => handleUpdate(accountId)}
-          style={{
-            padding: '5px 10px',
-            borderRadius: '5px',
-            border: 'none',
-            backgroundColor: '#007bff',
-            color: 'white',
-            cursor: 'pointer',
-          }}>
+        <Button variant="warning" onClick={() => openUpdateShow(accountId)}>
           Update
-        </button>
+        </Button>
       ),
     },
     {
@@ -106,48 +81,171 @@ const AccountsTables = () => {
           original: { accountId },
         },
       }) => (
-        <button
-          onClick={() => handleDelete(accountId)}
-          style={{
-            padding: '5px 10px',
-            borderRadius: '5px',
-            border: 'none',
-            backgroundColor: 'red',
-            color: 'white',
-            cursor: 'pointer',
-          }}>
+        <Button variant="danger" onClick={() => openDeleteShow(accountId)}>
           Delete
-        </button>
+        </Button>
       ),
     },
   ]
 
+  const [deleteShow, setDeleteShow] = useState(false)
+  const [updateShow, setUpdateShow] = useState(false)
+  const [createShow, setCreateShow] = useState(false)
+  const [newAccount, setNewAccount] = useState({
+    accountName: "",
+    accountPassword: "",
+    accountType: "",
+  })
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    setNewAccount({ ...newAccount, [name]: value })
+  }
+  const [input, setInput] = useState('')
+
+  const openCreateModal = () => {
+    setCreateShow(true)
+  }
+
+  const handleCreate = () => {
+    console.log(newAccount)
+  }
+
+  const openUpdateShow = (accountId) => {
+    setUpdateShow(true)
+    const account = accounts?.find((account) => account.accountId === accountId)
+    setInput(account)
+  }
+
+  const handleUpdate = (input) => {
+    console.log(input)
+  }
+
+  const openDeleteShow = (accountId) => {
+    setDeleteShow(true)
+    setInput(accountId)
+  }
+
+  const handleDelete = (e, input) => {
+    e.preventDefault()
+    fetch(`http://localhost:8080/accounts/${input}`, {
+      method: 'DELETE',
+    })
+      .then((response) => {
+        if (response.ok) {
+          setAccounts(accounts.filter((account) => account.accountId !== input))
+          setDeleteShow(false)
+        } else {
+          console.error('Failed to delete the account')
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      })
+  }
+
   return (
     <>
       <PageMetaData title="Account Tables" />
-      <div style={{ marginBottom: '20px', textAlign: 'center' }}>
-        <input
-          type="text"
-          placeholder="Search by account name"
-          value={searchTerm}
-          onChange={handleSearch}
-          style={{
-            padding: '10px',
-            width: '100%',
-            borderRadius: '5px',
-            border: '1px solid #ccc',
-            boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
-            fontSize: '16px',
-          }}
-        />
-      </div>
+      <Form.Control
+        className="p-3 mb-3 rounded"
+        size="lg"
+        type="text"
+        placeholder="Search by account name"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
       {filteredAccounts && (
         <Row className="justify-content-center">
           <Col xs={12}>
-            <CustomerDetailTable accounts={accounts} />
+            <CustomerDetailTable accounts={filteredAccounts} />
           </Col>
         </Row>
       )}
+
+      <Modal show={deleteShow} onHide={() => setDeleteShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setDeleteShow(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={(e) => handleDelete(e, input)}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={updateShow} onHide={() => setUpdateShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Woohoo, you are reading this text in a modal!
+          {/* <Form>
+            <Form.Control type="hidden" value={input} />
+          </Form> */}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setUpdateShow(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => handleUpdate(input)}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Form>
+      <Modal show={createShow} onHide={() => setCreateShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            
+            <Form.Group className="m-2">
+              <Form.Label>Account name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Account name"
+                name="accountName"
+                value={newAccount.accountName}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group className="m-2">
+              <Form.Label>Account password</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Account password"
+                name="accountPassword"
+                value={newAccount.accountPassword}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group className="m-2">
+              <Form.Label>Account type</Form.Label>
+              <Form.Select required name="accountType" value={newAccount.accountType} onChange={handleInputChange} className="bg-body text-dark border-secondary">
+                <option value="">Select account type</option>
+                <option value={1}>Customer</option>
+                <option value={2}>Staff</option>
+              </Form.Select>
+            </Form.Group>
+            
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setCreateShow(false)}>
+            Close
+          </Button>
+          <Button type="submit" variant="primary" onClick={() => handleCreate()}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      </Form>
     </>
   )
 }
