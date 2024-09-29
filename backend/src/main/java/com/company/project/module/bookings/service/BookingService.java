a3138c623a106c73121c565166108744fb4af5b6
namdng09 <namduong090104@gmail.com>
2024-09-29 21:17:31 +0700

    feat: New service for Booking

---
+++ backend/src/main/java/com/company/project/module/bookings/service/BookingService.java
@@ -0,0 +1,82 @@
+package com.company.project.module.bookings.service;
+
+import java.util.List;
+
+import com.company.project.common.Status;
+import com.company.project.module.bookings.common.BookingStatusMessage;
+import com.company.project.module.bookings.dto.request.BookingCreationRequest;
+import com.company.project.module.bookings.entity.Booking;
+import com.company.project.module.bookings.exception.BookingException;
+import com.company.project.module.bookings.repository.BookingRepository;
+import com.company.project.module.customers.entity.Customer;
+import com.company.project.module.customers.service.CustomerService;
+import com.company.project.module.showtimes.entity.Showtime;
+import com.company.project.module.showtimes.service.ShowtimeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+  
+  @Autowired
+  private BookingRepository bookingRepository;
+
+  @Autowired
+  private ShowtimeService showtimeService;
+
+  @Autowired
+  private CustomerService customerService;
+
+  public List<Booking> getAllBooking() {
+    return bookingRepository.findAll();
+  }
+  
+  public Booking getBookingById(String bookingId) {
+    return bookingRepository.findById(bookingId)
+    .orElseThrow(() -> new BookingException(
+      Status.FAIL.getValue(), 
+      BookingStatusMessage.NOT_EXIST.getMessage()));
+  }
+
+  public Booking createBooking(BookingCreationRequest request) {
+    Showtime showtime = showtimeService.getShowtimeById(request.getShowtimeId());
+    Customer customer = customerService.getCustomerById(request.getCustomerId());
+
+    Booking booking = Booking.builder()
+      .showtime(showtime)
+      .customer(customer)
+      .bookingPrice(request.getBookingPrice())
+      .build();
+
+    return bookingRepository.save(booking);
+  }
+
+  public Booking updateBooking(String bookingId, BookingCreationRequest request) {
+    if (!bookingRepository.existsById(bookingId)) {
+      throw new BookingException(Status.FAIL.getValue(), BookingStatusMessage.NOT_EXIST.getMessage());
+    }
+
+    Showtime showtime = showtimeService.getShowtimeById(request.getShowtimeId());
+    Customer customer = customerService.getCustomerById(request.getCustomerId());
+
+    Booking existedBooking = this.getBookingById(bookingId);
+    existedBooking.setShowtime(showtime);
+    existedBooking.setCustomer(customer);
+    existedBooking.setBookingPrice(request.getBookingPrice());
+
+    return bookingRepository.save(existedBooking);
+  }
+
+  public void deleteBookingById(String bookingId) {
+    if (!bookingRepository.existsById(bookingId)) {
+      throw new BookingException(Status.FAIL.getValue(), BookingStatusMessage.NOT_EXIST.getMessage());
+    }
+
+    bookingRepository.deleteById(bookingId);
+  }
+
+}
+
+
+
