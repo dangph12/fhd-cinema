5839ae5c1d4a072fd2743e35223dc3bdcc195caa
namdng09 <namduong090104@gmail.com>
2024-09-29 21:14:23 +0700

    feat: Booking controller

---
+++ backend/src/main/java/com/company/project/module/bookings/controller/BookingController.java
@@ -0,0 +1,93 @@
+package com.company.project.module.bookings.controller;
+
+import java.util.List;
+
+import jakarta.validation.Valid;
+
+import com.company.project.common.ApiResponse;
+import com.company.project.common.Status;
+import com.company.project.module.bookings.common.BookingStatusMessage;
+import com.company.project.module.bookings.dto.request.BookingCreationRequest;
+import com.company.project.module.bookings.entity.Booking;
+import com.company.project.module.bookings.service.BookingService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bookings")
+public class BookingController {
+  
+  @Autowired
+  private BookingService bookingService;
+
+  @GetMapping()
+  ResponseEntity<ApiResponse<List<Booking>>> getAllBooking() {
+    return ResponseEntity.status(HttpStatus.OK.value())
+    .body(ApiResponse.<List<Booking>>builder()
+      .status(Status.SUCCESS.getValue())
+      .message(BookingStatusMessage.GET_SUCCESS.getMessage())
+      .data(bookingService.getAllBooking())
+      .build());
+  }
+
+  @GetMapping("/{bookingId}")
+  ResponseEntity<ApiResponse<Booking>> getBookingById(
+    @PathVariable(name = "bookingId") String bookingId) {
+    return ResponseEntity.status(HttpStatus.OK.value())
+    .body(ApiResponse.<Booking>builder()
+      .status(Status.SUCCESS.getValue())
+      .message(BookingStatusMessage.GET_SUCCESS.getMessage())
+      .data(bookingService.getBookingById(bookingId))
+      .build());
+  }
+  
+  @PostMapping
+  ResponseEntity<ApiResponse<Booking>> addBooking(
+    @RequestBody @Valid BookingCreationRequest request) {
+    Booking booking =  bookingService.createBooking(request);
+
+    return ResponseEntity.status(HttpStatus.CREATED.value())
+    .body(ApiResponse.<Booking>builder()
+      .status(Status.SUCCESS.getValue())
+      .message(BookingStatusMessage.CREATE_SUCCESS.getMessage())
+      .data(booking)
+      .build());
+  }
+
+  @PutMapping("/{bookingId}")
+  ResponseEntity<ApiResponse<Booking>> updateBooking(
+    @PathVariable(name = "bookingId") String bookingId,
+    @Valid @RequestBody BookingCreationRequest request) {
+    Booking booking = bookingService.updateBooking(bookingId, request);
+
+    return ResponseEntity.status(HttpStatus.OK.value())
+      .body(ApiResponse.<Booking>builder()
+        .status(Status.SUCCESS.getValue())
+        .message(BookingStatusMessage.UPDATE_SUCCESS.getMessage())
+        .data(booking)
+        .build());
+  }
+
+  @DeleteMapping("/{bookingId}")
+  ResponseEntity<ApiResponse<Void>> deleteBooking(
+    @PathVariable(name = "bookingId") String bookingId) {
+    bookingService.deleteBookingById(bookingId);
+
+    return ResponseEntity.status(HttpStatus.OK.value())
+      .body(ApiResponse.<Void>builder()
+        .status(Status.SUCCESS.getValue())
+        .message(BookingStatusMessage.DELETE_SUCCESS.getMessage())
+        .build());
+  }
+
+}
