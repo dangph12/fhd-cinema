85fc6c86f1dd57fcb34b4c9c183f40a082fc0750
namdng09 <namduong090104@gmail.com>
2024-09-29 21:14:11 +0700

    feat: Enum class for Booking status

---
+++ backend/src/main/java/com/company/project/module/bookings/common/BookingStatusMessage.java
@@ -0,0 +1,28 @@
+package com.company.project.module.bookings.common;
+
+import lombok.Getter;
+
+@Getter
+public enum BookingStatusMessage {
+
+  EMPTY_NAME("The name of booking must not be empty"),
+  EMPTY_SHOWTIME("The Showtime Id must not be empty"),
+  EMPTY_CUSTOMER("The Customer Id must not be empty"),
+  NULL_BOOKED("The isBooked must not be empty"),
+  NOT_EXIST("The booking Id does not exist"),
+  EXIST_ID("The booking's Id have been existed"),
+  EXIST_BOOKING("The booking have been existed"),
+  CREATE_SUCCESS("The booking has been created successfully"),
+  GET_SUCCESS("Get booking successfully"),
+  DELETE_SUCCESS("The booking deleted successfully"),
+  UPDATE_SUCCESS("The booking updated successfully");
+
+  private final String message;
+
+  BookingStatusMessage(String message) {
+    this.message = message;
+  }
+
+}
+
+
