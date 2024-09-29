1deea7c1c4c3b94168c1a4f88cfac3d1dde2fd05
namdng09 <namduong090104@gmail.com>
2024-09-29 21:16:10 +0700

    refactor: change the path of libralies

--- backend/src/main/java/com/company/project/module/bookings/entity/Booking.java
+++ backend/src/main/java/com/company/project/module/bookings/entity/Booking.java
@@ -1,19 +1,36 @@
 package com.company.project.module.bookings.entity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
 import com.company.project.module.bills.entity.Bill;
 import com.company.project.module.customers.entity.Customer;
 import com.company.project.module.showtimes.entity.Showtime;
 import com.company.project.module.snacks.entity.Snack;
 import com.company.project.module.tickets.entity.Ticket;
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import lombok.*;
-import lombok.experimental.FieldDefaults;
+
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 @Entity
 @Data
