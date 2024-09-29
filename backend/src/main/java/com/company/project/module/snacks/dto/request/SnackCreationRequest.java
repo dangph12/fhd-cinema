f727484fddf1bff006596ad0d4520f1320cf2a3a
namdng09 <namduong090104@gmail.com>
2024-09-29 23:02:42 +0700

    feat: New DTO class for create request Snack

---
+++ backend/src/main/java/com/company/project/module/snacks/dto/request/SnackCreationRequest.java
@@ -0,0 +1,26 @@
+package com.company.project.module.snacks.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SnackCreationRequest {
+
+  @NotEmpty(message = "EMPTY_NAME")
+  String snackName;
+
+  @Min(value = 0, message = "NEGATIVE_VALUE")
+  int snackPrice;  
+
+}
