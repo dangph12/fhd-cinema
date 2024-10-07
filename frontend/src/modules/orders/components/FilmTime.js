634ab8f7396d72690a033c556e5828cf46c577db
lnvh <lenguyenviethung1509@gmail.com>
2024-10-07 22:34:37 +0700

    Nothing change to commit

--- frontend/src/modules/orders/components/FilmTime.js
+++ frontend/src/modules/orders/components/FilmTime.js
@@ -194,7 +194,7 @@ const FilmTime = () => {
 
     // Hàm điều hướng đến trang chọn ghế với showtimeId và showTimeAt
     const goToSeatSelection = (showtimeId) => {
-        navigate(`/seatselection/${showtimeId}`); // Navigate to SeatSelection with showtimeId
+        navigate(`/seatSelection/${showtimeId}`); // Navigate to SeatSelection with showtimeId
     };
 
     useEffect(() => {
