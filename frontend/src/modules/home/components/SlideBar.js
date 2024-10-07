29b17039dfa446488a6046649125c48dbcaf4598
Trang Vu <trangvlhe181187@fpt.edu.vn>
2024-10-07 11:03:25 +0700

    fix: slide bar (#52)

--- frontend/src/modules/home/components/SlideBar.js
+++ frontend/src/modules/home/components/SlideBar.js
@@ -4,7 +4,6 @@ import 'react-multi-carousel/lib/styles.css';
 function SlideBar() {
     const responsive = {
         superLargeDesktop: {
-            // the naming can be any, depends on you.
             breakpoint: { max: 4000, min: 1024 },
             items: 3,
             slideToSlide: 3
@@ -24,54 +23,39 @@ function SlideBar() {
     };
     return (
         <div>
-            <div class="grid">
-                <div class="hot-series">
-                    <div class="hot-label">
-                        <h2 class="title-heading">
-                            <h3 class="hot-series">Hot Series</h3>
+            <div className="grid">
+                <div className="hot-series">
+                    <div className="hot-label">
+                        <h2 className="title-heading">
+                            <h2 className="hot-series">Hot Series</h2>
                         </h2>
                     </div>
                     <Carousel responsive={responsive}>
                         <div className="card">
                             <img className="product--image" src="https://bhdstar.vn/wp-content/uploads/2024/09/referenceSchemeHeadOfficeallowPlaceHoldertrueheight700ldapp-9.jpg" />
-                            {/* <h2>Ma Da</h2>
-                            <p className="price">$14</p> */}
                         </div>
                         <div className="card">
                             <img className="product--image" src="https://bhdstar.vn/wp-content/uploads/2024/09/referenceSchemeHeadOfficeallowPlaceHoldertrueheight700ldapp-2.jpg" />
-                            {/* <h2>Ma Da</h2>
-                            <p className="price">$14</p> */}
                         </div>
                         <div className="card">
                             <img className="product--image" src="https://bhdstar.vn/wp-content/uploads/2024/09/referenceSchemeHeadOfficeallowPlaceHoldertrueheight700ldapp-10.jpg" />
-                            {/* <h2>Ma Da</h2>
-                            <p className="price">$14</p> */}
                         </div>
                         <div className="card">
                             <img className="product--image" src="https://bhdstar.vn/wp-content/uploads/2024/08/referenceSchemeHeadOfficeallowPlaceHoldertrueheight700ldapp-3.png" />
-                            {/* <h2>Ma Da</h2>
-                            <p className="price">$14</p> */}
                         </div>
                         <div className="card">
                             <img className="product--image" src="https://bhdstar.vn/wp-content/uploads/2024/09/referenceSchemeHeadOfficeallowPlaceHoldertrueheight700ldapp-13.jpg" />
-                            {/* <h2>Ma Da</h2>
-                            <p className="price">$14</p> */}
                         </div>
                         <div className="card">
                             <img className="product--image" src="https://bhdstar.vn/wp-content/uploads/2024/09/referenceSchemeHeadOfficeallowPlaceHoldertrueheight700ldapp.png" />
-                            {/* <h2>Ma Da</h2>
-                            <p className="price">$14</p> */}
                         </div>
                         <div className="card">
                             <img className="product--image" src="https://bhdstar.vn/wp-content/uploads/2024/08/referenceSchemeHeadOfficeallowPlaceHoldertrueheight700ldapp-21.jpg" />
-                            {/* <h2>Ma Da</h2>
-                            <p className="price">$14</p> */}
                         </div>
-                    </Carousel>;
+                    </Carousel>
                 </div>
             </div>
         </div>
     )
 }
-export default SlideBar
-
+export default SlideBar;
