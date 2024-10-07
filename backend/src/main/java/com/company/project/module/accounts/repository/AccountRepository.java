acbb6e1d92b63491e81c3186d1def99132495cbe
namdng09 <namduong090104@gmail.com>
2024-10-07 22:14:45 +0700

    feat: Function to check exist Account by Name

--- backend/src/main/java/com/company/project/module/accounts/repository/AccountRepository.java
+++ backend/src/main/java/com/company/project/module/accounts/repository/AccountRepository.java
@@ -1,16 +1,14 @@
 package com.company.project.module.accounts.repository;
 
 import com.company.project.module.accounts.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
     boolean existsByAccountId(String accountId);
-
+    boolean existsByAccountName(String accountName);
 //    @Query("select a from Account a join fetch a.customer ac")
 //    List<Account> findAll();
 }
