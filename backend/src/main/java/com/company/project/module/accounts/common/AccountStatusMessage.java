8b518e78637a863460b3e63aa650bf5fa75dbb4d
namdng09 <namduong090104@gmail.com>
2024-10-07 22:12:43 +0700

    fix: Change enum message for get success

--- backend/src/main/java/com/company/project/module/accounts/common/AccountStatusMessage.java
+++ backend/src/main/java/com/company/project/module/accounts/common/AccountStatusMessage.java
@@ -8,10 +8,11 @@ public enum AccountStatusMessage {
     DELETE_SUCCESS("The account was deleted successfully"),
     UPDATE_SUCCESS("The account was updated successfully"),
     NOT_EXIST("The account is not exist"),
+    EXIST_NAME("The account's name have been existed"),
     STRING_VALUE("The account's price must be a valid number"),
     NEGATIVE_VALUE("The variable must be greater than 0"),
     CREATE_SUCCESS("The account was created successfully"),
-    GET_SUCCESS("Get all accounts successfully"),
+    GET_SUCCESS("Get accounts successfully"),
     EMPTY_NAME("The account name must not be empty"),
     EMPTY_PASSWORD("The account password must not be empty");
 
