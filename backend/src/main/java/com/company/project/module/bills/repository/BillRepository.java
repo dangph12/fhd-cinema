e892631ce9d4f012912b429de4ab78bd85dc43f6
namdng09 <namduong090104@gmail.com>
2024-10-07 22:17:58 +0700

    feat: Bill repository

--- backend/src/main/java/com/company/project/module/bills/repository/BillRepository.java
+++ backend/src/main/java/com/company/project/module/bills/repository/BillRepository.java
@@ -1,6 +1,9 @@
 package com.company.project.module.bills.repository;
 
+import java.util.List;
+
 import com.company.project.module.bills.entity.Bill;
+import com.company.project.module.vouchers.entity.Voucher;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BillRepository extends JpaRepository<Bill, String>{
   boolean existsByBooking_BookingId(String bookingId);
+  List<Bill> findByVouchersContaining(Voucher voucher);
 }
