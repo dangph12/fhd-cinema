9518dd6caedfab9460856b15ea93445f610cc486
namdng09 <namduong090104@gmail.com>
2024-10-08 20:48:06 +0700

    feat: New method in email controller to reset password (#55)

--- backend/src/main/java/com/company/project/module/emails/controller/EmailController.java
+++ backend/src/main/java/com/company/project/module/emails/controller/EmailController.java
@@ -7,6 +7,7 @@ import com.company.project.common.ApiResponse;
 import com.company.project.common.Status;
 import com.company.project.module.emails.common.EmailStatusMessage;
 import com.company.project.module.emails.dto.request.EmailBillRequest;
+import com.company.project.module.emails.dto.request.EmailResetPasswordRequest;
 import com.company.project.module.emails.service.EmailService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,16 @@ public class EmailController{
   @GetMapping("/bill")
   ResponseEntity<ApiResponse<Void>> sendBillInformationEmail(@Valid @RequestBody EmailBillRequest request) {
     emailService.sendEmailBill(request);
+
+    return ResponseEntity.ok().body(ApiResponse.<Void>builder()
+      .status(Status.SUCCESS.getValue())
+      .message(EmailStatusMessage.EMAIL_SENT_SUCCESSFULLY.getMessage())
+      .build());
+  }
+
+  @GetMapping("/reset-password")
+  ResponseEntity<ApiResponse<Void>> sendEmailResetPassword(@Valid @RequestBody EmailResetPasswordRequest request) {
+    emailService.sendEmailResetPassword(request);
 
     return ResponseEntity.ok().body(ApiResponse.<Void>builder()
       .status(Status.SUCCESS.getValue())
