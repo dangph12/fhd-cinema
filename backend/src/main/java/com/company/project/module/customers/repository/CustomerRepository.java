742767451da66abfa7ea57f311c3eae9f296b558
namdng09 <namduong090104@gmail.com>
2024-10-14 16:19:40 +0700

    feat: Add password to update account

--- backend/src/main/java/com/company/project/module/customers/repository/CustomerRepository.java
+++ backend/src/main/java/com/company/project/module/customers/repository/CustomerRepository.java
@@ -1,13 +1,6 @@
 package com.company.project.module.customers.repository;
 
-<<<<<<< HEAD
 import com.company.project.module.customers.entity.Customer;
-=======
-import java.util.List;
-
-import com.company.project.module.customers.entity.Customer;
-import com.company.project.module.vouchers.entity.Voucher;
->>>>>>> 40436fe (fix: Using email instead of customerId to send email reset password (#55))
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -17,10 +10,6 @@ public interface CustomerRepository extends JpaRepository<Customer, String> {
   boolean existsByCustomerName(String customerName);
   boolean existsByCustomerPhone(String customerPhone);
   boolean existsByCustomerEmail(String customerEmail);
-<<<<<<< HEAD
-=======
-  List<Customer> findByVouchersContaining(Voucher voucher);
->>>>>>> 40436fe (fix: Using email instead of customerId to send email reset password (#55))
   Customer findByCustomerEmail(String customerEmail);
 }
 
