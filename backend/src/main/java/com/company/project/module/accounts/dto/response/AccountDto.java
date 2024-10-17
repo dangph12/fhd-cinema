6633fc939c1780d800941b770453a69b4163efd2
dangph12 <dangphhe181217@fpt.edu.vn>
2024-10-17 11:40:33 +0700

    Update AccountDto.java

--- backend/src/main/java/com/company/project/module/accounts/dto/response/AccountDto.java
+++ backend/src/main/java/com/company/project/module/accounts/dto/response/AccountDto.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:backend/src/main/java/com/company/project/module/accounts/dto/response/AccountDto.java
 package com.company.project.module.accounts.dto.response;
-========
-package com.company.project.module.accounts.dto.request;
->>>>>>>> 86a6b88dc763fbc8ee659a8d2d0965a1d076e552:backend/src/main/java/com/company/project/module/accounts/dto/request/AuthenticationRequest.java
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -12,11 +8,12 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class AuthenticationRequest {
+@Builder
+public class AccountDto {
+    String accountId;
     String accountName;
-    String accountPassword;
+    String accountType;
 }
