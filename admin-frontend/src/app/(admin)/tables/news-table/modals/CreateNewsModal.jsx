e19c6cc87b171ffb0082170656f326f3fdd703c8
dangph12 <dangphhe181217@fpt.edu.vn>
2024-10-20 10:21:16 +0700

    feat: Create news UI

--- admin-frontend/src/app/(admin)/tables/news-table/modals/CreateNewsModal.jsx
+++ admin-frontend/src/app/(admin)/tables/news-table/modals/CreateNewsModal.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Modal, Form, Button } from 'react-bootstrap'
 import { NewsContext } from '../context/NewsContext'
-
+import TextEditor from '../../common/TextEditor'
+import uploadToS3 from '../../common/UploadToS3'
 
 function CreateNewsModal({ show, fetchNews, onHide }) {
-
   const { state } = useContext(NewsContext)
   const [createShow, setCreateShow] = useState(false)
 
@@ -12,9 +12,16 @@ function CreateNewsModal({ show, fetchNews, onHide }) {
     setCreateShow(show)
   }, [show])
 
-  const [form, setForm] = useState({ newsName: '', newsType: '' })
+  const [form, setForm] = useState({
+    newsTitle: '',
+    newsDescription: '',
+    newsCreateAt: '',
+    newsImageUrl: '',
+    newsCategoryId: '',
+  })
   const [validated, setValidated] = useState(false)
   const [errors, setErrors] = useState({})
+  const [imageFile, setImageFile] = useState(null)
 
   const setField = (field, value) => {
     setForm({
@@ -26,49 +33,84 @@ function CreateNewsModal({ show, fetchNews, onHide }) {
   const closeCreateShow = () => {
     onHide()
     setCreateShow(false)
-    setForm({ newsName: '', newsPassword: '', newsType: '' })
+    setForm({
+      newsTitle: '',
+      newsDescription: '',
+      newsCreateAt: '',
+      newsImageUrl: '',
+      newsCategoryId: '',
+    })
     setValidated(false)
     setErrors({})
+    setImageFile(null)
   }
 
   const validateForm = () => {
     const newErrors = {}
-    if (!form.newsName) newErrors.newsName = 'News name is required'
-    if (!form.newsType) newErrors.newsType = 'News type is required'
+    if (form.newsTitle === '') {
+      newErrors.newsTitle = 'Title is required'
+    }
+    if (form.newsDescription === '') {
+      newErrors.newsDescription = 'Description is required'
+    }
+    if (form.newsCreateAt === '') {
+      newErrors.newsCreateAt = 'Create at is required'
+    }
+    if (imageFile === null) {
+      newErrors.newsImageUrl = 'Image is required'
+    }
+    if (form.newsCategoryId === '') {
+      newErrors.newsCategoryId = 'Category is required'
+    }
     return newErrors
   }
 
-  const handleCreate = (e) => {
+  const finishForm = async () => {
+    const date = new Date();
+    const dateStr = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}T00:00:00`;
+    form.newsCreateAt = dateStr;
+    
+    if (imageFile) {
+      const imageUrl = await uploadToS3('news', imageFile);
+      form.newsImageUrl = imageUrl;
+    }
+  };
+
+  const createNews = async () => {
+    fetch('http://localhost:8080/news', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    })
+      .then((response) => {
+        if (response.ok) {
+          fetchNews()
+        } else {
+          console.error('Failed to create the news')
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      })
+  }
+
+  const handleCreate = async (e) => {
     e.preventDefault()
+
+    await finishForm()
+    console.log(form) 
+    debugger
 
     const newErrors = validateForm()
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors)
       e.stopPropagation()
     } else {
-      fetch('http://localhost:8080/news', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(form),
-      })
-        .then((response) => {
-          if (response.ok) {
-            fetchNews()
-          } else {
-            console.error('Failed to create the news')
-          }
-        })
-        .catch((error) => {
-          console.error('Error:', error)
-        })
-      setCreateShow(false)
-      onHide()
-      setForm({ newsName: '', newsPassword: '', newsType: '' })
-      setErrors({})
+      await createNews()
+      closeCreateShow()
     }
-    setValidated(true)
   }
   return (
     <Modal show={createShow} onHide={() => closeCreateShow()}>
@@ -77,77 +119,44 @@ function CreateNewsModal({ show, fetchNews, onHide }) {
       </Modal.Header>
       <Modal.Body>
         <Form noValidate validated={validated} onSubmit={handleCreate} id="createForm">
-          <Form.Group className="m-2">
-            <Form.Label>News name</Form.Label>
+          <Form.Group controlId="formNewsTitle" className="m-2">
+            <Form.Label>Title</Form.Label>
             <Form.Control
-              required
               type="text"
-              onChange={(e) => setField('newsName', e.target.value)}
-              placeholder="News name"
-              name="newsName"
-              value={form.newsName}
-              isInvalid={!!errors.newsName}
+              placeholder="Enter title"
+              value={form.newsTitle}
+              onChange={(e) => setField('newsTitle', e.target.value)}
+              isInvalid={!!errors.newsTitle}
             />
-            <Form.Control.Feedback type="invalid">{errors.newsName}</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">{errors.newsTitle}</Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="m-2">
-            <Form.Label>News type</Form.Label>
+            <Form.Label>News description</Form.Label>
+            <TextEditor object="news" description={form.newsDescription} field="newsDescription" setField={setField} />
+          </Form.Group>
+          <Form.Group className="m-2">
+            <Form.Label>News Image</Form.Label>
+            <Form.Control type="file" accept="image/*" onChange={(e) => setImageFile(e.target.files[0])} isInvalid={!!errors.newsImageUrl} />
+            <Form.Control.Feedback type="invalid">{errors.newsImageUrl}</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="formNewsCategoryId" className="m-2">
+            <Form.Label>Category</Form.Label>
             <Form.Select
               required
-              name="newsType"
-              onChange={(e) => setField('newsType', e.target.value)}
+              name="newsCategoryId"
+              onChange={(e) => setField('newsCategoryId', e.target.value)}
               className="bg-body text-dark border-secondary"
-              value={form.newsType}
-              isInvalid={!!errors.newsType}>
-              <option value="">Select news type</option>
-              <option value={'Admin'}>Admin</option>
-              <option value={'Customer'}>Customer</option>
+              value={form.newsCategoryId}
+              isInvalid={!!errors.newsCategoryId}>
+              <option value="">Select news category </option>
+              {state.newsCategories.map((newsCategory) => (
+                <option key={newsCategory.newsCategoryId} value={newsCategory.newsCategoryId}>
+                  {newsCategory.newsCategoryName}
+                </option>
+              ))}
             </Form.Select>
-            <Form.Control.Feedback type="invalid">{errors.newsType}</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">{errors.newsCategoryId}</Form.Control.Feedback>
           </Form.Group>
-          {form.newsType == 'Customer' ? (
-            <>
-              <Form.Group className="m-2">
-                <Form.Label>Customer name</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  onChange={(e) => setField('customerName', e.target.value)}
-                  placeholder="Customer name"
-                  name="newsPassword"
-                  value={form.newsPassword}
-                  isInvalid={!!errors.newsPassword}
-                />
-                <Form.Control.Feedback type="invalid">{errors.newsPassword}</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="m-2">
-                <Form.Label>Customer Email</Form.Label>
-                <Form.Control
-                  required
-                  type="email"
-                  onChange={(e) => setField('customerEmail', e.target.value)}
-                  placeholder="Customer email"
-                  name="customerEmail"
-                  value={form.customerEmail}
-                  isInvalid={!!errors.customerEmail}
-                />
-                <Form.Control.Feedback type="invalid">{errors.customerEmail}</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="m-2">
-                <Form.Label>Customer Phone</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  onChange={(e) => setField('customerPhone', e.target.value)}
-                  placeholder="Customer phone"
-                  name="customerPhone"
-                  value={form.customerPhone}
-                  isInvalid={!!errors.customerPhone}
-                />
-                <Form.Control.Feedback type="invalid">{errors.customerPhone}</Form.Control.Feedback>
-              </Form.Group>
-            </>
-          ) : null}
         </Form>
       </Modal.Body>
       <Modal.Footer>
