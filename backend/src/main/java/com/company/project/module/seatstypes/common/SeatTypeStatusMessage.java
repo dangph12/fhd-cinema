598c235d0e12308fc13da4bbbb4ed34c2990f725
namdng09 <namduong090104@gmail.com>
2024-10-22 00:12:50 +0700

    feat: Paging for the SeatTypes

--- backend/src/main/java/com/company/project/module/seatstypes/common/SeatTypeStatusMessage.java
+++ backend/src/main/java/com/company/project/module/seatstypes/common/SeatTypeStatusMessage.java
@@ -9,6 +9,7 @@ public enum SeatTypeStatusMessage {
   UPDATE_SUCCESS("The seat type updated successfully"),
   EMPTY_TYPE_NAME("The name of seat's type must not be empty"),
   NOT_EXIST("The seat type's Id does not exist"),
+  LESS_THAN_ZERO("The index must not be less than 0"),
   EXIST_TYPE("The seat type have been existed"),
   STRING_VALUE("The ticket's price must be a valid number"),
   NEGATIVE_VALUE("The variable must be greater than 0"),
--- backend/src/main/java/com/company/project/module/seatstypes/service/SeatTypeService.java
+++ backend/src/main/java/com/company/project/module/seatstypes/service/SeatTypeService.java
@@ -2,6 +2,7 @@ package com.company.project.module.seatstypes.service;
 
 import java.util.List;
 
+import com.company.project.common.ApiPagination;
 import com.company.project.common.Status;
 import com.company.project.module.seatstypes.common.SeatTypeStatusMessage;
 import com.company.project.module.seatstypes.dto.request.SeatTypeCreationRequest;
@@ -10,6 +11,10 @@ import com.company.project.module.seatstypes.exception.SeatTypeException;
 import com.company.project.module.seatstypes.repository.SeatTypeRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -65,6 +70,27 @@ public class SeatTypeService {
     }
 
     seatTypeRepository.deleteById(seatTypeId);
+  }
+
+  public ApiPagination<SeatType> filterSeatTypes(String seatTypeName, int page, int pageSize,
+        String sortBy, String sortDirection) {
+    if (page < 1 || pageSize < 1) {
+      throw new SeatTypeException(Status.FAIL.getValue(), SeatTypeStatusMessage.LESS_THAN_ZERO.getMessage());
+    }
+
+    Sort.Direction direction = Sort.Direction.fromString(sortDirection);
+
+    Pageable pageable = PageRequest.of(page - 1, pageSize, Sort.by(direction, sortBy));
+
+    Page<SeatType> seatTypePages = seatTypeRepository.findBySeatTypeNameContainingIgnoreCase(seatTypeName, pageable);
+    long count = seatTypeRepository.countBySeatTypeNameContainingIgnoreCase(seatTypeName);
+
+    ApiPagination<SeatType> seatTypePagination = ApiPagination.<SeatType>builder()
+        .result(seatTypePages.getContent())
+        .count(count)
+        .build();
+    
+    return seatTypePagination;
   }
 
 }
