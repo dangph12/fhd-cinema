e6b5519dd3a7a4908e8da1731fdfb003bdb076f9
namdng09 <namduong090104@gmail.com>
2024-10-22 00:51:14 +0700

    feat: Paging for the Snacks

--- backend/src/main/java/com/company/project/module/snacks/repository/SnackRepository.java
+++ backend/src/main/java/com/company/project/module/snacks/repository/SnackRepository.java
@@ -2,10 +2,14 @@ package com.company.project.module.snacks.repository;
 
 import com.company.project.module.snacks.entity.Snack;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SnackRepository extends JpaRepository<Snack, String> {
   boolean existsBySnackName(String snackName);
+  Page<Snack> findBySnackNameContainingIgnoreCase(String ratingName, Pageable pageable);
+  long countBySnackNameContainingIgnoreCase(String ratingName);
 }
