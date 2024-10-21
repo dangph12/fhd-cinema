7d8decfc652c224e553332510a50cf1090cd680b
namdng09 <namduong090104@gmail.com>
2024-10-21 22:38:47 +0700

    feat: Paging for ratings

--- backend/src/main/java/com/company/project/module/ratings/controller/RatingController.java
+++ backend/src/main/java/com/company/project/module/ratings/controller/RatingController.java
@@ -1,17 +1,28 @@
 package com.company.project.module.ratings.controller;
 
+import java.util.List;
+
+import jakarta.validation.Valid;
+
+import com.company.project.common.ApiPagination;
 import com.company.project.common.ApiResponse;
 import com.company.project.common.Status;
 import com.company.project.module.ratings.common.RatingStatusMessage;
 import com.company.project.module.ratings.dto.request.RatingCreationRequest;
 import com.company.project.module.ratings.entity.Rating;
 import com.company.project.module.ratings.service.RatingService;
-import jakarta.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ratings")
@@ -42,6 +53,20 @@ public class RatingController {
                         .message(RatingStatusMessage.GET_SUCCESS.getMessage())
                         .data(rating)
                         .build());
+    }
+
+    @GetMapping(params = "search")
+    ResponseEntity<ApiResponse<ApiPagination<Rating>>> filterMovies(
+        @RequestParam(value = "search") String search,
+        @RequestParam(value = "page", defaultValue = "1") int page,
+        @RequestParam(value = "sortBy", defaultValue = "ratingName") String sortBy, 
+        @RequestParam(value = "sortDirection", defaultValue = "ASC") String sortDirection,
+        @RequestParam(value = "pageSize", defaultValue = "2") int pageSize) {
+      return ResponseEntity.ok().body(ApiResponse.<ApiPagination<Rating>>builder()
+              .status(Status.SUCCESS.getValue())
+              .message(RatingStatusMessage.GET_SUCCESS.getMessage())
+              .data(ratingService.filterMovies(search, page, pageSize, sortBy, sortDirection))
+              .build());
     }
 
     @PostMapping
--- backend/src/main/java/com/company/project/module/ratings/service/RatingService.java
+++ backend/src/main/java/com/company/project/module/ratings/service/RatingService.java
@@ -1,14 +1,22 @@
 package com.company.project.module.ratings.service;
 
+import java.util.List;
+
+import com.company.project.common.ApiPagination;
 import com.company.project.common.Status;
+import com.company.project.module.movies.common.MovieStatusMessage;
+import com.company.project.module.movies.exception.MovieException;
 import com.company.project.module.ratings.common.RatingStatusMessage;
 import com.company.project.module.ratings.dto.request.RatingCreationRequest;
 import com.company.project.module.ratings.entity.Rating;
 import com.company.project.module.ratings.exception.RatingException;
 import com.company.project.module.ratings.repository.RatingRepository;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
 @Service
 public class RatingService {
@@ -25,6 +33,27 @@ public class RatingService {
 
     public Rating getRatingById(String ratingId) {
         return ratingRepository.findById(ratingId).orElseThrow(() -> new RatingException(Status.FAIL.getValue(), RatingStatusMessage.NOT_EXIST.getMessage()));
+    }
+
+    public ApiPagination<Rating> filterMovies(String ratingName, int page, int pageSize,
+          String sortBy, String sortDirection) {
+      if (page < 1 || pageSize < 1) {
+        throw new MovieException(Status.FAIL.getValue(), MovieStatusMessage.LESS_THAN_ZERO.getMessage());
+      }
+
+      Sort.Direction direction = Sort.Direction.fromString(sortDirection);
+
+      Pageable pageable = PageRequest.of(page - 1, pageSize, Sort.by(direction, sortBy));
+
+      Page<Rating> ratingPages = ratingRepository.findByRatingNameContainingIgnoreCase(ratingName, pageable);
+      long count = ratingRepository.countByRatingNameContainingIgnoreCase(ratingName);
+
+      ApiPagination<Rating> ratingPagination = ApiPagination.<Rating>builder()
+          .result(ratingPages.getContent())
+          .count(count)
+          .build();
+      
+      return ratingPagination;
     }
 
     public Rating createRating(RatingCreationRequest request) {
