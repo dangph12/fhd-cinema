c25d0f6e2a98d6cf066f7488da7d8e6a4b66c94c
namdng09 <namduong090104@gmail.com>
2024-10-22 23:10:34 +0700

    feat: Paging for the bills

--- backend/src/main/java/com/company/project/module/bills/common/BillStatusMessage.java
+++ backend/src/main/java/com/company/project/module/bills/common/BillStatusMessage.java
@@ -11,6 +11,8 @@ public enum BillStatusMessage {
   EMPTY_ISPAID("The paid must not be empty"),
   NEGATIVE_VALUE("The variable must be greater than 0"),
   NOT_EXIST("The bill does not exist"),
+  UNKNOWN_ATTRIBUTE("The attribute does not exist"),
+  LESS_THAN_ZERO("The index must not be less than 0"),
   EXIST_BOOKING("The booking of bill have been existed"),
   GET_SUCCESS("Get bill successfully"),
   CREATE_SUCCESS("The bill has been created successfully"),
