8c3f86cd889f8a0091697d18a1e02faa4460d7d3
dangph12 <dangphhe181217@fpt.edu.vn>
2024-10-22 16:23:42 +0700

    feat: Init vnpay

---
+++ backend/src/main/java/com/company/project/vnpay/dto/request/TransactionRequest.java
@@ -0,0 +1,14 @@
+package com.company.project.vnpay.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TransactionRequest {
+    int orderTotal;
+    String orderInfo;
+}
