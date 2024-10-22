ce540d4845407016a8fd789d989f4f16aafd690d
namdng09 <namduong090104@gmail.com>
2024-10-22 19:08:50 +0700

    feat: Paging for the vouchers

--- backend/src/main/java/com/company/project/module/vouchers/service/VoucherService.java
+++ backend/src/main/java/com/company/project/module/vouchers/service/VoucherService.java
@@ -2,6 +2,7 @@ package com.company.project.module.vouchers.service;
 
 import java.util.List;
 
+import com.company.project.common.ApiPagination;
 import com.company.project.common.Status;
 import com.company.project.module.bills.service.BillService;
 import com.company.project.module.vouchers.common.VoucherStatusMessage;
@@ -9,8 +10,13 @@ import com.company.project.module.vouchers.dto.request.VoucherCreationRequest;
 import com.company.project.module.vouchers.entity.Voucher;
 import com.company.project.module.vouchers.exception.VoucherException;
 import com.company.project.module.vouchers.repository.VoucherRepository;
+import com.company.project.utils.Utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +28,9 @@ public class VoucherService {
 
   @Autowired
   private BillService billService;
+
+  @Autowired
+  private Utils utils;
 
   public List<Voucher> getAllVoucher() {
     return voucherRepository.findAll();
@@ -103,6 +112,33 @@ public class VoucherService {
     billService.removeVoucherInBill(voucher);
 
     voucherRepository.delete(voucher);
+  }
+
+  public ApiPagination<Voucher> filterVouchers(String voucherName, int page, int pageSize,
+        String sortBy, String sortDirection) {
+    if (page < 1 || pageSize < 1) {
+      throw new VoucherException(Status.FAIL.getValue(), VoucherStatusMessage.LESS_THAN_ZERO.getMessage());
+    }
+
+    List<String> voucherFieldNames = utils.getEntityFields(Voucher.class);
+
+    if (!voucherFieldNames.contains(sortBy)) {
+      throw new VoucherException(Status.FAIL.getValue(), VoucherStatusMessage.UNKNOWN_ATTRIBUTE.getMessage());
+    }
+
+    Sort.Direction direction = Sort.Direction.fromString(sortDirection);
+
+    Pageable pageable = PageRequest.of(page - 1, pageSize, Sort.by(direction, sortBy));
+
+    Page<Voucher> voucherPages = voucherRepository.findByVoucherNameContainingIgnoreCase(voucherName, pageable);
+    long count = voucherRepository.countByVoucherNameContainingIgnoreCase(voucherName);
+
+    ApiPagination<Voucher> voucherPagination = ApiPagination.<Voucher>builder()
+        .result(voucherPages.getContent())
+        .count(count)
+        .build();
+    
+    return voucherPagination;
   }
 
 }
