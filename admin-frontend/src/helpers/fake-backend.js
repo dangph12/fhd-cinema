ad94136ba5b6d071931972d468dec0e41a999b9d
dangph12 <dangphhe181217@fpt.edu.vn>
2024-10-27 11:19:52 +0700

    feat: change log in to username

--- admin-frontend/src/helpers/fake-backend.js
+++ admin-frontend/src/helpers/fake-backend.js
@@ -29,8 +29,9 @@ export default function configureFakeBackend() {
         const params = JSON.parse(config.data);
         // find if any user matches login credentials
         const filteredUsers = fakeUsers.filter(user => {
-          return user.email === params.email && user.password === params.password;
+          return user.username === params.email && user.password === params.password;
         });
+        debugger
         if (filteredUsers.length) {
           // if login details are valid return user details and fake jwt token
           const user = filteredUsers[0];
