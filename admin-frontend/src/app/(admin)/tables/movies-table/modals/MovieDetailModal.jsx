0dd200dc7cc41ffbc930e5d2c3703dc3ce8da85e
ducndahe181048 <144119087+ducndahe181048@users.noreply.github.com>
2024-11-05 14:47:35 +0700

    refactor: fullscreen to modal in movies

--- admin-frontend/src/app/(admin)/tables/movies-table/modals/MovieDetailModal.jsx
+++ admin-frontend/src/app/(admin)/tables/movies-table/modals/MovieDetailModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { Modal, Form, Button } from 'react-bootstrap'
+import { Modal, Form, Button, Container, Row, Col } from 'react-bootstrap'
 import { MovieContext } from '../context/MovieContext'
 
 function MovieDetailModal({ movieId, show, onHide }) {
@@ -67,64 +67,102 @@ function MovieDetailModal({ movieId, show, onHide }) {
   }
 
   return (
-    <Modal show={detailShow} onHide={() => closeDetailShow()}>
+    <Modal fullscreen={true} show={detailShow} onHide={() => closeDetailShow()}>
       <Modal.Header closeButton>
         <Modal.Title>Detail Modal</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form id="detailForm">
-        <Form.Group className="m-2">
-            <Form.Label>Rating</Form.Label>
-            <Form.Control readOnly type="text" value={selectedMovie.ratingName} />
-          </Form.Group>
-          <Form.Group className="m-2">
-            <Form.Label>Movie Title</Form.Label>
-            <Form.Control readOnly type="text" value={selectedMovie.movieTitle} />
-          </Form.Group>
-          <Form.Group className="m-2">
-            <Form.Label>Movie Genre</Form.Label>
-            <Form.Control type="text" readOnly name="movieGenre" className="bg-body text-dark border-secondary" value={selectedMovie.movieGenre} />
-          </Form.Group>
-          <Form.Group className="m-2">
-            <Form.Label>Movie Director</Form.Label>
-            <Form.Control type="text" readOnly name="movieDirector" className="bg-body text-dark border-secondary" value={selectedMovie.movieDirector} />
-          </Form.Group>
-          <Form.Group className="m-2">
-            <Form.Label>Movie Cast</Form.Label>
-            <Form.Control type="text" readOnly name="movieCast" className="bg-body text-dark border-secondary" value={selectedMovie.movieCast} />
-          </Form.Group>
-          <Form.Group className="m-2">
-            <Form.Label>Movie Status</Form.Label>
-            <Form.Control type="text" readOnly name="movieStatus" className="bg-body text-dark border-secondary" value={selectedMovie.movieStatus} />
-          </Form.Group>
-          <Form.Group className="m-2">
-            <Form.Label>Movie Format</Form.Label>
-            <Form.Control type="text" readOnly name="movieFormat" className="bg-body text-dark border-secondary" value={selectedMovie.movieFormat} />
-          </Form.Group>
-          <Form.Group className="m-2">
-            <Form.Label>Movie Duration Minute</Form.Label>
-            <Form.Control type="number" readOnly name="movieDurationMinute" className="bg-body text-dark border-secondary" value={selectedMovie.movieDurationMinute} />
-          </Form.Group>
-          <Form.Group className="m-2">
-            <Form.Label>Movie Release Date</Form.Label>
-            <Form.Control type="date" readOnly name="movieReleaseDate" className="bg-body text-dark border-secondary" value={selectedMovie.movieReleaseDate} />
-          </Form.Group>
-          <Form.Group className="m-2">
-            <Form.Label>Movie Trailer URL</Form.Label>
-            <Form.Control type="text" readOnly name="movieTrailerUrl" className="bg-body text-dark border-secondary" value={selectedMovie.movieTrailerUrl} />
-          </Form.Group>
-          <Form.Group className="m-2">
-            <Form.Label>Movie Description</Form.Label>
-            <Form.Control as="textarea" rows={3} readOnly name="movieDescription" className="bg-body text-dark border-secondary" value={selectedMovie.movieDescription} />
-          </Form.Group>
-          <Form.Group className="m-2">
-            <Form.Label>Movie Language</Form.Label>
-            <Form.Control type="text" readOnly name="movieLanguage" className="bg-body text-dark border-secondary" value={selectedMovie.movieLanguage} />
-          </Form.Group>
-          <Form.Group className="m-2">
-            <Form.Label>Movie Poster URL</Form.Label>
-            <Form.Control type="text" readOnly name="moviePosterUrl" className="bg-body text-dark border-secondary" value={selectedMovie.moviePosterUrl} />
-          </Form.Group>
+          <Container>
+            <Row>
+              <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Rating</Form.Label>
+                  <Form.Control readOnly type="text" value={selectedMovie.ratingName} />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Movie Title</Form.Label>
+                  <Form.Control readOnly type="text" value={selectedMovie.movieTitle} />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Movie Genre</Form.Label>
+                  <Form.Control type="text" readOnly name="movieGenre" className="bg-body text-dark border-secondary" value={selectedMovie.movieGenre} />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Movie Director</Form.Label>
+                  <Form.Control type="text" readOnly name="movieDirector" className="bg-body text-dark border-secondary" value={selectedMovie.movieDirector} />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Movie Cast</Form.Label>
+                  <Form.Control type="text" readOnly name="movieCast" className="bg-body text-dark border-secondary" value={selectedMovie.movieCast} />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Movie Status</Form.Label>
+                  <Form.Control type="text" readOnly name="movieStatus" className="bg-body text-dark border-secondary" value={selectedMovie.movieStatus} />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Movie Format</Form.Label>
+                  <Form.Control type="text" readOnly name="movieFormat" className="bg-body text-dark border-secondary" value={selectedMovie.movieFormat} />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Movie Duration Minute</Form.Label>
+                  <Form.Control type="number" readOnly name="movieDurationMinute" className="bg-body text-dark border-secondary" value={selectedMovie.movieDurationMinute} />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Movie Release Date</Form.Label>
+                  <Form.Control type="date" readOnly name="movieReleaseDate" className="bg-body text-dark border-secondary" value={selectedMovie.movieReleaseDate} />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Movie Trailer URL</Form.Label>
+                  <Form.Control type="text" readOnly name="movieTrailerUrl" className="bg-body text-dark border-secondary" value={selectedMovie.movieTrailerUrl} />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Movie Language</Form.Label>
+                  <Form.Control type="text" readOnly name="movieLanguage" className="bg-body text-dark border-secondary" value={selectedMovie.movieLanguage} />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Movie Poster URL</Form.Label>
+                  <Form.Control type="text" readOnly name="moviePosterUrl" className="bg-body text-dark border-secondary" value={selectedMovie.moviePosterUrl} />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+            <Col>
+                <Form.Group className="m-2">
+                  <Form.Label>Movie Description</Form.Label>
+                  <Form.Control as="textarea" rows={3} readOnly name="movieDescription" className="bg-body text-dark border-secondary" value={selectedMovie.movieDescription} />
+                </Form.Group>
+              </Col>
+            </Row>
+          </Container>
         </Form>
       </Modal.Body>
       <Modal.Footer></Modal.Footer>
